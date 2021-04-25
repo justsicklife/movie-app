61d437c020bd5a045cdcdccc4e57d9c85d6dec83
justsicklife <justsicklife@gmail.com>
2021-04-26 01:40:21 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors')
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const url = process.env.MONGO_URL;
+
+const cookieParser = require("cookie-parser");
+
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(url,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const corsOptions = {
+  origin: `http://localhost:${process.env.CLIENT_PORT}`
+}
+
+app.use(cors(corsOptions))
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(express.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', require('./routes/users'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`)
+});
