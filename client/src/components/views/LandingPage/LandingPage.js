b8d79315b3dd53e6873086f5c1689764f3adda21
justsicklife <justsicklife@gmail.com>
2021-05-01 03:46:56 +0900

    LandingPage 새로 구축

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,50 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+import {
+    API_URL,
+    API_KEY,
+} from '../../Config.js';
+import GridCards from './section/GridCards/GridCards.js';
+import MainImage from './section/MainImage/MainImage.js';
+import {IMAGE_BASE_URL} from '../../Config.js';
 
 function LandingPage() {
 
+    const [Movies, setMovies] = useState([])
+    const [PageNumber, setPageNumber] = useState(1)
+    const [MainMovie, setMainMovie] = useState(null);
+
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${PageNumber}`;
+    
+    const onClickViewMore = () => {
+        setPageNumber(PageNumber+1);
+    }
+
+    const requestMovie = () => {
+        axios.get(endpoint)
+        .then(response => {
+            console.log(response.data.results);
+            setMainMovie(response.data.results[0]);
+            setMovies([...Movies,...response.data.results])
+        })
+    }
+
+    useEffect(() => {
+        requestMovie();
+    }, [PageNumber])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div className="landingp_page">
+            {MainMovie && 
+                <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovie.backdrop_path}`}/>
+            }
+            {Movies &&
+                <GridCards movies={Movies}/>
+            }
+            <button onClick={onClickViewMore}>
+                불러오기
+            </button>
+        </div>
     )
 }
 
