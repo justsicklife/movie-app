6774adbadf40cd6e5106447c4b56948bc81d3a2e
justsicklife <justsicklife@gmail.com>
2021-05-01 03:52:35 +0900

    config api 불러오기 위한 변수 추가

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,15 @@
-//SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const FAVORITE_SERVER = '/api/favorite';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = "c0ba4ee4a92cd6f989a38d9c317a5c02"
+export  const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
